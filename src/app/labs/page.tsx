6147269fa7f1f4457af8048c9c0b3d3ca66bf0d0
ViharReddy <44550857+ViharReddy@users.noreply.github.com>
2024-09-17 18:15:34 -0400

    updated landing page to also include links to github and respective labs directly

--- src/app/labs/page.tsx
+++ src/app/labs/page.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 export default function Labs() {
   return (
     <div id="wd-labs">
-      <h1>Labs</h1>
+      <h1>Sai Vihar Reddy Gunamgari</h1>
+      <h2>Fall 2024 Web Dev SEC 01</h2>
+      <h2>Labs</h2>
       <ul>
-        <li><Link href="/labs">Labs</Link></li>
         <li><Link href="/labs/lab1">Lab 1</Link></li>
         <li><Link href="/labs/lab2">Lab 2</Link></li>
         <li><Link href="/labs/lab3">Lab 3</Link></li>
         <li><Link href="/kanbas">Kanbas</Link></li>
+        <li><Link id="wd-github" href="https://github.com/ViharReddy/kanbas-react-nextjs-web-app">GitHub</Link></li>
       </ul>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,32 @@ export default function Home() {
   return (
     <div>
       <h1>Sai Vihar Reddy Gunamgari</h1>
-      <h2>Fall&apos;24 Web Dev SEC 01</h2>
+      <h2>Fall 2024 Web Dev SEC 01</h2>
       <ul>
         <li>
           <Link href="/labs">Labs</Link>
+          <ul>
+            <li>
+              <Link href="/labs/lab1">Lab 1</Link>
+            </li>
+            <li>
+              <Link href="/labs/lab2">Lab 2</Link>
+            </li>
+            <li>
+              <Link href="/labs/lab3">Lab 3</Link>
+            </li>
+          </ul>
         </li>
         <li>
           <Link href="/kanbas">Kanbas</Link>
+        </li>
+        <li>
+          <Link
+            id="wd-github"
+            href="https://github.com/ViharReddy/kanbas-react-nextjs-web-app"
+          >
+            GitHub
+          </Link>
         </li>
       </ul>
     </div>
